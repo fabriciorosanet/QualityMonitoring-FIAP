d13a14aaf49a9911136dc99bf1d58cf3e63e8065
fabriciorosanet <fabriciorosafiap@gmail.com>
2024-10-18 13:05:09 -0300

    teste ci-cd

--- obj/Debug/net8.0/QualityMonitoring-MongoDB.AssemblyInfo.cs
+++ obj/Debug/net8.0/QualityMonitoring-MongoDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QualityMonitoring-MongoDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e820cd9e1b3ba6b9a84740b73e13b13ff2df97e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3066fd21c47953460249fa76c8e9e8c9b6f6f125")]
 [assembly: System.Reflection.AssemblyProductAttribute("QualityMonitoring-MongoDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QualityMonitoring-MongoDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
